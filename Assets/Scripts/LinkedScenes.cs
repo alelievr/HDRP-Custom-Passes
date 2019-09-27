fdd22f4aa102a45e4b5c1473833c552368e3af81
Remy Maetz <remym@unity3d.com>
2019-09-27 16:57:37 +0200

    Fix SNAPS assets by exporting probuild mesh to assets for better instanciation.

---
+++ Assets/Scripts/LinkedScenes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LinkedScenes : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField]
+#endif
+
+    SceneAsset[] linkedScenes;
+    
+#if UNITY_EDITOR
+    void OnEnable()
+    {
+        if (linkedScenes != null && linkedScenes.Length > 0)
+        {
+            for (int i = 0; i < linkedScenes.Length; ++i)
+            {
+                EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(linkedScenes[i]), OpenSceneMode.Additive);
+            }
+
+            EditorSceneManager.SetActiveScene(EditorSceneManager.GetSceneAt(0));
+        }
+    }
+
+#endif
+    
+}
