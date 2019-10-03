77da6e12e4a0f00288ba0eec2e374bcfc68d6221
Remy Maetz <remym@unity3d.com>
2019-10-03 16:13:03 +0200

    Finished AR scene

---
+++ Assets/CustomPasses/AR/SyncVideoAndAnim.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+[RequireComponent(typeof(Animation))]
+public class SyncVideoAndAnim : MonoBehaviour
+{
+    VideoPlayer videoPlayer;
+    Animation animation;
+
+    bool firstUpdate = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        animation = GetComponent<Animation>();
+
+        animation.Play();
+
+        videoPlayer.Play();
+        videoPlayer.loopPointReached += Loop;
+    }
+
+    // Force a sync of the animation at the first rendered frame
+    void Update()
+    {
+        if (firstUpdate)
+        {
+            var t = (float) videoPlayer.time;
+            
+            foreach (AnimationState state in animation)
+            {
+                state.time = t;
+            }
+            
+            firstUpdate = false;
+        }
+    }
+
+    // Force a sync of the animation each time the video player loops
+    void Loop(VideoPlayer vp)
+    {
+        foreach (AnimationState state in animation)
+        {
+            state.time = 0;
+        }
+    }
+}
