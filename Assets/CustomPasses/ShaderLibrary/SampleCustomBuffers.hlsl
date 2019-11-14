7700c1dda0ca4d7456d17f3353b3855fb7c16d7a
Remy Maetz <remym@unity3d.com>
2019-11-14 10:16:21 +0100

    Fix warnings in shadergraph

--- Assets/CustomPasses/ShaderLibrary/SampleCustomBuffers.hlsl
+++ Assets/CustomPasses/ShaderLibrary/SampleCustomBuffers.hlsl
@@ -1,4 +1,6 @@
+#if SHADERGRAPH_PREVIEW == 0
 #include "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/RenderPass/CustomPass/CustomPassCommon.hlsl"
+#endif
 
 void SampleCustomColor_float(float2 UV, out float4 Color)
 {
